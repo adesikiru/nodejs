c95c3de38145f1496a4d9fd55cc8033669e99d7b
Ade2002 <sikirusulaimon2002@gmail.com>
2022-09-22 22:05:27 +0100

    nodejs

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http');
-
+const home = require('index.html');
 const server = http.createServer((req, res) => {
-    console.log(req);
-    /* process.exit(); */
+    /* console.log(req); */
+    process.exit();
 });
 server.listen(3000);
