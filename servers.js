6290695dc20d5a8ae74b5120cef0f80b57b7f226
Ade2002 <sikirusulaimon2002@gmail.com>
2022-09-25 03:23:36 +0100

    update

--- servers.js
+++ servers.js
@@ -1,6 +1,14 @@
 const http = require('http')
 const server = http.createServer(function (req, res) {
-  console.log(req.headers)
-  console.log(res)
+  console.log(req.headers, req.url, req.method)
+  res.setHeader('Content-Type', 'text/html')
+  res.write(`<html lang="en">`)
+  res.write(`<head><meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>`)
+  res.write(`<body>Ade is cool!!</body>`)
+  res.write(`</html>`)
 })
 server.listen(3003)
