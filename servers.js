eb5a1c9c43c19e7b15a0148a6bf30770db855abd
Ade2002 <sikirusulaimon2002@gmail.com>
2022-12-02 08:09:23 +0100

    update

--- servers.js
+++ servers.js
@@ -1,21 +1,26 @@
 const http = require('http')
 const server = http.createServer(function (req, res) {
-    /* console.log(req.headers, req.url, req.method) */
-    const url = req.url;
-    url === '/' ? res.write(`<html>
+  /* console.log(req.headers, req.url, req.method) */
+  const url = req.url;
+  if (url === '/') {
+    res.write(`<html>
     <head><title>Submit</title></head>
     <body>
     <form action='/message' method='POST'><input type='text' name='message' /> <button type='submit'>Submit</button></form></body>
-    </html>`) : '';
-
- /*  res.setHeader('Content-Type', 'text/html')
-  res.write(`<html lang="en">`)
-  res.write(`<head><meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>`)
-  res.write(`<body>Ade is cool!!</body>`)
-  res.write(`</html>`) */
+    </html>`);
+    return res.end
+  }
+  /*  ?
+    : '';
+*/
+  /*  res.setHeader('Content-Type', 'text/html')
+   res.write(`<html lang="en">`)
+   res.write(`<head><meta charset="UTF-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>Document</title>
+ </head>`)
+   res.write(`<body>Ade is cool!!</body>`)
+   res.write(`</html>`) */
 })
 server.listen(3003)
